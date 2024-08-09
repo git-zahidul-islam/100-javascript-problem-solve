6cfbdf03a4b68d55f5cd9b19a2a6110f153e2638
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-08-09 16:22:48 +0600

    problem1 solve

---
+++ problem1/problem1.js
@@ -0,0 +1,12 @@
+function arrayReplace(inputArray, elementToReplace, subtractionElem){
+    inputArray.forEach((element,index)=>{
+        if(elementToReplace == element){
+            inputArray[index] = subtractionElem;
+        };
+    })
+    console.log(inputArray);
+}
+
+arrayReplace([500, 23, 50],500,3)
+
+// solved

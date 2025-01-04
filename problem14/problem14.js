d7ab28c7c2ebe87170080f4644107be0525ac7f8
git-zahidul-islam <freelancerzahidf101@gmail.com>
2025-01-04 13:02:20 +0600

    added another questions

--- problem14/problem14.js
+++ problem14/problem14.js
@@ -10,3 +10,15 @@ console.log(countCharacter("hello world", "o")); // Output: 2
 
 // new branch ---- 01... 
 // main test
+
+// Check if a String is a Palindrome
+// Question: Write a function to check if a given string is a palindrome.
+// Solution:
+
+function isPalindrome(str) {
+    const cleanedStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    const reversedStr = cleanedStr.split('').reverse().join('');
+    return cleanedStr === reversedStr;
+}
+console.log(isPalindrome("A man, a plan, a canal, Panama")); // Output: true
+console.log(isPalindrome("hello")); // Output: false
